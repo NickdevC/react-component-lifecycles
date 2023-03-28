fface64cfbc1e5a3810d2a74d65c02420fa9829c
NickdevC <nick_coles@msn.com>
2023-03-28 13:23:37 +0000

    Add getData method and call it using componentDidMount

--- my-app/src/components/LifeCyclesCDM.js
+++ my-app/src/components/LifeCyclesCDM.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
 
 export class LifeCyclesCDM extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         data: 'loading'
+      }
+    }
+    getData() {
+        setTimeout(() => {
+            this.setState({
+                data: 'loaded'
+            })
+        }, 3000)
+    }
+    componentDidMount() {
+        this.getData()
+    }
     render() {
         return (
             <div>
-                <h1>LifeCyclesCDM</h1>
+                <h1>{this.state.data}</h1>
             </div>
         )
     }
