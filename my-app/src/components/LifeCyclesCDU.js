ce58e2e6de0ee9f7106b5941365f7905a572f062
NickdevC <nick_coles@msn.com>
2023-03-28 14:03:22 +0000

    Add LifeCyclesCDUChild component syntax from CI repo

--- my-app/src/components/LifeCyclesCDU.js
+++ my-app/src/components/LifeCyclesCDU.js
@@ -2,20 +2,25 @@ import React, { Component } from 'react'
 
 export class LifeCyclesCDU extends Component {
     constructor(props) {
-        
+        console.log('Constructor called')
         super(props)
         this.state = {
             greeting: 'Hello!'
         }
     }
     updateGreeting = () => {
+        console.log('update greeting called')
         this.setState(prevState => {
             return {
                 greeting: prevState.greeting === 'Hello!' ? 'Goodbye!': 'Hello!'
             }
         })
     }
+    componentDidUpdate() {
+        console.log('Component updated')
+    }
     render() {
+        console.log('Render method called')
         return (
             <div>
                 <h1>{this.state.greeting}</h1>
