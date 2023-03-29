ef556441914b55d606b7a245a718511a68ea7e32
NickdevC <nick_coles@msn.com>
2023-03-29 11:31:06 +0000

    Add LifeCyclesCWU and LifeCyclesCWUTracker components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 // import LifeCyclesCDM from './components/LifeCyclesCDM';
-import LifeCyclesCDU from './components/LifeCyclesCDU';
+// import LifeCyclesCDU from './components/LifeCyclesCDU';
+import LifeCyclesCWU from './components/LifeCyclesCWU';
 
 function App() {
   return (
     <div className="App">
-      <LifeCyclesCDU />
+      <LifeCyclesCWU />
     </div>
   );
 }
